983ba30752d158bd79125f04a11d6eb551d97248
Marcos Araujo <marcosaraujo2015.1@gmail.com>
2024-08-16 08:49:46 -0300

    new update

--- exercicio02_DOM/script10.js
+++ exercicio02_DOM/script10.js
@@ -1,35 +1,55 @@
 document.addEventListener('DOMContentLoaded', function(){
     
-    document.getElementById('moverParaDireitaBtn').addEventListener('click', moverDireita);
-    document.getElementById('moverParaEsquerdaBtn').addEventListener('click', moverEsquerda);
+    let ativosDisponiveis = document.getElementById('ativosDisponiveis');
+    let carteiraInvestimentos = document.getElementById('carteiraInvestimentos');
+    let btnDireita = document.getElementById('moverParaDireitaBtn');
+    let btnEsquerda = document.getElementById('moverParaEsquerdaBtn');
+    let collectionDisponiveis = ativosDisponiveis.selectedOptions;
+    let collectionInvestimentos = carteiraInvestimentos.selectedOptions;
+
+   
+    btnDireita.addEventListener('click', function(){
+        moverDireita(ativosDisponiveis, carteiraInvestimentos, collectionDisponiveis,btnDireita,btnEsquerda)
+    });
+    
+    btnEsquerda.addEventListener('click', function(){
+        moverEsquerda(ativosDisponiveis, carteiraInvestimentos,collectionInvestimentos,btnDireita,btnEsquerda)
+    });
 
 });
 
 
-function moverDireita(){    
-    let ativosDisponiveis = document.getElementById('ativosDisponiveis');
-    let collectionDisponiveis = ativosDisponiveis.selectedOptions;
-    let carteiraInvestimentos = document.getElementById('carteiraInvestimentos');
+function moverDireita(ativosDisponiveis, carteiraInvestimentos, collectionDisponiveis, btnDireita,btnEsquerda){      
    
     if(collectionDisponiveis.length == 0){
         alert("Nenhum disponível selecionado")
-          
     }
     else{
-        let novo = ativosDisponiveis.removeChild(collectionDisponiveis[0])
-        carteiraInvestimentos.appendChild(novo)
+        
+        carteiraInvestimentos.appendChild(ativosDisponiveis.removeChild(collectionDisponiveis[0]))
     }
+    
+    if(ativosDisponiveis.childElementCount == 0){
+        btnDireita.disabled = true;
+    }
+    else if(carteiraInvestimentos.childElementCount != 0){
+        btnEsquerda.disabled=false;
+    } 
 }
 
 
-function moverEsquerda(){
-    let collectionInvestimentos = carteiraInvestimentos.selectedOptions;
+function moverEsquerda(ativosDisponiveis, carteiraInvestimentos,collectionInvestimentos,btnDireita, btnEsquerda){
     
-    if(collectionInvestimentos.length == 0){
+     if(collectionInvestimentos.length == 0){
         alert("Nenhum investimento selecionado")
     }
     else{
-        let novo = carteiraInvestimentos.removeChild(collectionInvestimentos[0])
-        ativosDisponiveis.appendChild(novo)
+        ativosDisponiveis.appendChild(carteiraInvestimentos.removeChild(collectionInvestimentos[0]))
+    }
+    if(carteiraInvestimentos.childElementCount == 0){
+        btnEsquerda.disabled = true;
+    }
+    if(ativosDisponiveis.childElementCount != 0){
+        btnDireita.disabled=false;
     }
 }
